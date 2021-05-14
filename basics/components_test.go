da78ee539cf461c9cdfea057e27bcc5860edc269
Paul Ebose <paulebose@gmail.com>
2021-05-14 18:16:07 +0100

    add the first test for `basics` package
    - TestAdd

---
+++ basics/components_test.go
@@ -0,0 +1,22 @@
+package basics
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	cases := []struct {
+		in   []int
+		want int
+	}{
+		{[]int{1, 2, 3, 4, 5}, 15},
+		{[]int{1, 2, 3, 4, 5, 6, 7, 8}, 36},
+		{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 55},
+		{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13}, 91},
+		{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}, 120},
+	}
+	for _, c := range cases {
+		got := Add(c.in...)
+		if got != c.want {
+			t.Errorf("Add(%v) got %v instead of %v", c.in, got, c.want)
+		}
+	}
+}
